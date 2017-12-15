68621dcbef0267c1ecacf7e1412d9b2ab3631f8e
Kieran Healy <kjhealy@gmail.com>
2017-12-15 10:27:38 -0500

    clean organdata names

--- R/data.r
+++ R/data.r
@@ -209,22 +209,22 @@
 #' \item year. Year.
 #' \item donors. Organ Donation rate per million population.
 #' \item pop. Population in thousands.
-#' \item pop.dens. Population density per square mile.
+#' \item pop_dens. Population density per square mile.
 #' \item gdp. Gross Domestic Product in thousands of PPP dollars.
-#' \item gdp.lag. Lagged Gross Domestic Product in thousands of PPP dollars.
+#' \item gdp_lag. Lagged Gross Domestic Product in thousands of PPP dollars.
 #' \item health. Health spending, thousands of PPP dollars per capita.
-#' \item health.lag Lagged health spending, thousands of PPP dollars
+#' \item health_lag Lagged health spending, thousands of PPP dollars
 #' per capita.
 #' \item pubhealth. Public health spending as a percentage of total expenditure.
 #' \item roads. Road accident fatalities per 100,000 population.
 #' \item cerebvas. Cerebrovascular deaths per 100,000 population (rounded).
 #' \item assault. Assault deaths per 100,000 population (rounded).
 #' \item external. Deaths due to external causes per 100,000 population.
-#' \item txp.pop. Transplant programs per million population.
+#' \item txp_pop. Transplant programs per million population.
 #' \item world. Welfare state world (Esping Andersen.)
 #' \item opt. Opt-in policy or Opt-out policy.
-#' \item consent.law. Consent law, informed or presumed.
-#' \item consent.practice. Consent practice, informed or presumed.
+#' \item consent_law. Consent law, informed or presumed.
+#' \item consent_practice. Consent practice, informed or presumed.
 #' \item consistent. Law consistent with practice, yes or no.
 #' \item ccode. Abbreviated country code.
 #' }
