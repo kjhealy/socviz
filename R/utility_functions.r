84b393d24b7af70ae321830420a63a37fc74ae2b
Kieran Healy <kjhealy@gmail.com>
2018-01-15 14:38:31 -0500

    add theme_map

--- R/utility_functions.r
+++ R/utility_functions.r
@@ -471,3 +471,29 @@ prefix_replace <- function(var_names, prefixes, replacements, toTitle = TRUE, ..
   }
   if(toTitle) tools::toTitleCase(out) else out
 }
+
+##' Mostly blank ggplot2 theme for use with maps
+##'
+##' A ggplot2 theme
+##' @title theme_map
+##' @param base_size Base font size
+##' @param base_family Base font family
+##' @return A ggplot theme layout
+##' @author Bob Rudis
+##' @export
+theme_map <- function(base_size=9, base_family="") {
+    require(grid)
+    theme_bw(base_size=base_size, base_family=base_family) %+replace%
+        theme(axis.line=element_blank(),
+              axis.text=element_blank(),
+              axis.ticks=element_blank(),
+              axis.title=element_blank(),
+              panel.background=element_blank(),
+              panel.border=element_blank(),
+              panel.grid=element_blank(),
+              panel.spacing=unit(0, "lines"),
+              plot.background=element_blank(),
+              legend.justification = c(0,0),
+              legend.position = c(0,0)
+              )
+}
